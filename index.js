2f17d761069ef81f18b13b545d0273b447851f83
arunraj-ar <mail.arunrajar@gmail.com>
2023-08-02 01:31:53 +0530

    added show more option

---
+++ index.js
@@ -0,0 +1,10 @@
+const showMore = () => {
+  document.getElementById("intro").innerHTML =
+    "<p>I'm Arun, a front-end developer and UI/UX enthusiast with a passion for crafting captivating user experiences. Experienced in React, Javascript, LightningJS, CSS, and HTML, I bring life to pixels. Beyond code, I adore  graphic design and have a tech obsession, from PCs to gadgets and video  games. Let's reshape the digital world together! Oh, and a little  secret: ChatGPT was kind enough to assist with this intro - programming   genius, not a writing whiz<p/>";
+};
+const showLess = () => {
+  document.getElementById("intro").innerHTML =
+    "<p>I'm Arun, a front-end developer and UI/UX enthusiast with a passion for crafting captivating user experiences...</p>";
+};
+document.getElementById("intro").addEventListener("focus", showMore);
+document.getElementById("intro").addEventListener("blur", showLess);
